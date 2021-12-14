7023ea9b7c15ed5f8b5e069edb13b461a6a0fcd6
Hitesh Sankhat <hitesh.sankhat@zeuslearning.com>
2021-12-14 23:21:14 +0530

    rename files

---
+++ 2.opened-closed-principle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2.opened_closed_principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
